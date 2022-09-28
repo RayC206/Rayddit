f82dc8acbb9a287b0ac4f546b1474128f4d7ffcf
RayC206 <rcv.henry@gmail.com>
2022-09-28 09:02:22 -0700

    Beginning on working user and subreddit route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -4,7 +4,7 @@ from app.models import User
 
 user_routes = Blueprint('users', __name__)
 
-
+# Get all users
 @user_routes.route('/')
 @login_required
 def users():
@@ -12,8 +12,18 @@ def users():
     return {'users': [user.to_dict() for user in users]}
 
 
+# Get details of a user
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+# Get all subreddits user is subscribed to
+
+
+# Get all posts for a user's subscriptions (home)
+
+
+# Get all posts authored by a user (profile)
