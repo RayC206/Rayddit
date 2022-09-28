2c514dd89558b3ef52eb38f63095ffb7112ca067
RayC206 <rcv.henry@gmail.com>
2022-09-27 21:26:50 -0700

    post routes complete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -22,10 +22,18 @@ def get_all_posts():
 @post_routes.route("/<int:post_id>")
 def post_details(post_id):
   post = Post.query.get(post_id)
+
   if not post:
     return {"message": "Post does not exist"}
-  else:
-    return post.to_dict()
+
+  total_votes = 0
+  for vote in post.votes:
+    total_votes += vote.value
+
+  post = post.to_dict()
+  post["total_votes"] = total_votes
+
+  return post
 
 # Create a post
 @post_routes.route("/", methods=["POST"])
@@ -33,7 +41,6 @@ def post_details(post_id):
 def create_post():
   form = PostForm()
   form['csrf_token'].data = request.cookies['csrf_token']
-  print(form.data)
   if form.validate_on_submit():
     new_post = Post(
       title = form.data["title"],
@@ -51,3 +58,88 @@ def create_post():
     return new_post
   else:
     return jsonify(form.errors)
+
+## Edit a post
+@post_routes.route("/<int:post_id>", methods=["PUT"])
+@login_required
+def edit_post(post_id):
+  form = PostForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  edited_post = Post.query.get(post_id)
+  print("-----",current_user.id)
+  if current_user.id != edited_post.user_id:
+    return {"message": "You must be the owner of this post to edit", "statusCode": 403}
+
+  if form.validate_on_submit():
+    edited_post.title = form.data['title']
+    edited_post.img_url = form.data['img_url']
+    edited_post.link_url = form.data['link_url']
+    edited_post.text = form.data['text']
+
+    db.session.commit()
+
+    return edited_post.to_dict()
+  else:
+    return jsonify(form.errors)
+
+## Delete a post
+@post_routes.delete("/<int:post_id>")
+@login_required
+def delete_post(post_id):
+  post = Post.query.get(post_id)
+  if current_user.id == post.user_id:
+    db.session.delete(post)
+    db.session.commit()
+    return {'message': 'Successfully deleted post'}
+  else:
+    return {'message': 'Only post author can delete post', 'statusCode': 403}
+
+## Upvote a post
+@post_routes.route("/<int:post_id>/upvote", methods=["POST"])
+@login_required
+def upvote_post(post_id):
+  post = Post.query.get(post_id)
+  if not post:
+    return {"message": "Post does not exist"}
+
+  vote = Vote.query.filter(Vote.user_id == current_user.id, Vote.post_id == post_id).first()
+  if not vote:
+    upvote = Vote(
+        user_id = current_user.id,
+        post_id = post_id,
+        value = 1
+      )
+    db.session.add(upvote)
+  else:
+    if vote.value == 1:
+      vote.value = 0
+    else:
+      vote.value = 1
+
+  db.session.commit()
+  return {"message": "Upvote success"}
+
+# Downvote a post
+@post_routes.route("/<int:post_id>/downvote", methods=["POST"])
+@login_required
+def downvote_post(post_id):
+  post = Post.query.get(post_id)
+  if not post:
+    return {"message": "Post does not exist"}
+
+  vote = Vote.query.filter(Vote.user_id == current_user.id, Vote.post_id == post_id).first()
+  if not vote:
+    downvote = Vote(
+        user_id = current_user.id,
+        post_id = post_id,
+        value = -1
+      )
+    db.session.add(downvote)
+  else:
+    if vote.value == -1:
+      vote.value = 0
+    else:
+      vote.value = -1
+
+  db.session.commit()
+  return {"message": "Downvote success"}
