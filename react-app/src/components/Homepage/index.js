e8bfed8a49d051ae8cf6db38a2ec179963153b4a
RayC206 <rcv.henry@gmail.com>
2022-10-01 14:04:13 -0700

    finally got side container aligned on homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -52,72 +52,85 @@ const Homepage = () => {
   };
 
   return (
-    <div className="homePageDiv">
-      <div className="createPostDiv">
-        <div className="createInputContainer">
-          <input
-            type="text"
-            placeholder="Create Post"
-            className="inputBox"
-            onClick={createPostPage}
-          />
-        </div>
-      </div>
-      {postsLoaded ? (
-        posts.length ? (
-          posts.map((post) => {
-            return (
-              <div className="outerPostContainer" key={post.id}>
-                <div className="voteDiv">
-                  <button onClick={() => upvotePost(post.id)}>Up</button>
-                  {post.total_votes}
-                  <button onClick={() => downvotePost(post.id)}>Down</button>
-                </div>
-                <div
-                  className="postContainer"
-                  // onClick={(e) => postDetailPage(post.id)}
-                >
-                  <div className="postTopDescription">
-                    <div className="postSubredditName">
-                      r/{post.subreddit_name}
+    <div className="pageContainer">
+      <div className="homePageDiv">
+        <div className="rowOne">
+          <div className="createPostDiv">
+            <div className="createInputContainer">
+              <input
+                type="text"
+                placeholder="Create Post"
+                className="inputBox"
+                onClick={createPostPage}
+              />
+            </div>
+          </div>
+          {postsLoaded ? (
+            posts.length ? (
+              posts.map((post) => {
+                return (
+                  <div className="outerPostContainer" key={post.id}>
+                    <div className="voteDiv">
+                      <button onClick={() => upvotePost(post.id)}>Up</button>
+                      {post.total_votes}
+                      <button onClick={() => downvotePost(post.id)}>
+                        Down
+                      </button>
                     </div>
                     <div
-                      className="postUsername"
-                      onClick={(e) => usersProfilePage(post.user_id)}
+                      className="postContainer"
+                      // onClick={(e) => postDetailPage(post.id)}
                     >
-                      u/{post.username}
+                      <div className="postTopDescription">
+                        <div className="postSubredditName">
+                          r/{post.subreddit_name}
+                        </div>
+                        <div
+                          className="postUsername"
+                          onClick={(e) => usersProfilePage(post.user_id)}
+                        >
+                          u/{post.username}
+                        </div>
+                        <div className="postTimeago">
+                          {post.created_at_timeago}
+                        </div>
+                      </div>
+                      <div className="postTitle">{post.title}</div>
+                      <div
+                        className="postContent"
+                        onClick={(e) => postDetailPage(post.id)}
+                      >
+                        {(() => {
+                          if (post.post_type_id === POST_TYPE_TEXT) {
+                            return <div className="postText">{post.text}</div>;
+                          } else if (post.post_type_id === POST_TYPE_IMAGE) {
+                            return (
+                              <img className="postImage" src={post.img_url} />
+                            );
+                          } else if (post.post_type_id === POST_TYPE_LINK) {
+                            return (
+                              <a className="postLinkurl" href={post.link_url}>
+                                {post.link_url}
+                              </a>
+                            );
+                          }
+                        })()}
+                      </div>
                     </div>
-                    <div className="postTimeago">{post.created_at_timeago}</div>
                   </div>
-                  <div className="postTitle">{post.title}</div>
-                  <div
-                    className="postContent"
-                    onClick={(e) => postDetailPage(post.id)}
-                  >
-                    {(() => {
-                      if (post.post_type_id === POST_TYPE_TEXT) {
-                        return <div className="postText">{post.text}</div>;
-                      } else if (post.post_type_id === POST_TYPE_IMAGE) {
-                        return <img className="postImage" src={post.img_url} />;
-                      } else if (post.post_type_id === POST_TYPE_LINK) {
-                        return (
-                          <a className="postLinkurl" href={post.link_url}>
-                            {post.link_url}
-                          </a>
-                        );
-                      }
-                    })()}
-                  </div>
-                </div>
-              </div>
-            );
-          })
-        ) : (
-          <div>No posts yet</div>
-        )
-      ) : (
-        <div>Loading...</div>
-      )}
+                );
+              })
+            ) : (
+              <div>No posts yet</div>
+            )
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
+        <div className="rowTwo">
+          <div className="subredditSuggestions"></div>
+        </div>
+      </div>
     </div>
   );
 };
