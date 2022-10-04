3928e1de633ca77d5505009c1b843f38ea761981
RayC206 <rcv.henry@gmail.com>
2022-10-03 23:09:56 -0700

    added default user image

--- app/models/users.py
+++ app/models/users.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    profile_image = db.Column(db.String)
+    profile_image = db.Column(db.String, default="https://c.tenor.com/59l1VjOQOocAAAAC/reddit-profile.gif")
     hashed_password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, onupdate=func.now())
