8bacce093376c64ba827cd79cac42a1ee0c5ef1c
RayC206 <rcv.henry@gmail.com>
2022-10-10 00:54:13 -0700

    changed banner image to objectFit, changed validation message on login, pls dont break

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -4,12 +4,12 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 
-def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('Email provided not found.')
+# def user_exists(form, field):
+#     # Checking if user exists
+#     email = field.data
+#     user = User.query.filter(User.email == email).first()
+#     if not user:
+#         raise ValidationError()
 
 
 def password_matches(form, field):
@@ -18,12 +18,12 @@ def password_matches(form, field):
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
     if not user:
-        raise ValidationError('No such user exists.')
+        raise ValidationError('You have entered an invalid Email or Password.')
     if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+        raise ValidationError('You have entered an invalid Email or Password.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), Email("Email address is not valid") ])
     password = StringField('password', validators=[
                            DataRequired(), password_matches])
